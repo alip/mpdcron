83645ca597243187e2f9280ecc3637a1168b8025
Ali Polatel <alip@exherbo.org>
2009-12-18 11:17:23 +0200

    Add example database hook

---
+++ conf/hooks/dump-stats.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# vim: set sw=4 et sts=4 tw=80 :
+# Copyright 2009 Ali Polatel <alip@exherbo.org>
+# Distributed under the terms of the GNU General Public License v2
+
+echo "=== MPD STATS ==="
+echo "Update Time: $MPD_DATABASE_UPDATE_TIME"
+echo "Artists: $MPD_DATABASE_ARTISTS"
+echo "Albums: $MPD_DATABASE_ALBUMS"
+echo "Songs: $MPD_DATABASE_SONGS"
+echo "Play time: $MPD_DATABASE_PLAY_TIME"
+echo "Uptime: $MPD_DATABASE_UPTIME"
+echo "Database Play time: $MPD_DATABASE_DB_PLAY_TIME"
+echo "================="
